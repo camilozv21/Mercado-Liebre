d1c84224397ca4152cf24a9cbcee311a6f94c780
camilozv21 <camilozv21@gmail.com>
2023-05-08 19:32:11 -0500

    Cambios

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Servidor levantado en el puerto 3000 🚀')); //Windows punto 
+app.listen(port, () => console.log('Servidor levantado en el puerto' * port *' 🚀')); //Windows punto 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
