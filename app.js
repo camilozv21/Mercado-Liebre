ca480cc5d0b81afbc920a037fb826e064a1b0aa2
camilozv21 <camilozv21@gmail.com>
2023-05-08 20:07:38 -0500

    Comit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log('Servidor levantado en el puerto' * port *' 🚀')); //Windows punto 
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto '  ${PORT}🚀`)); //Windows punto 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
@@ -19,3 +19,4 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'))
 });
+// `` altgr + }
